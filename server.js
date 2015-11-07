ef966aadb3ba7bb3357027b0b0822e3120a98a33
Eduardo Alves <contato@edualves.net>
2015-11-07 12:54:28 -0200

    Update(Node server) to export tests

--- server.js
+++ server.js
@@ -2,10 +2,19 @@
 * Created by eduardo on 30/10/15.
 */
 var express = require('express'),
+ bodyParser = require('body-parser'),
  app     = express(),
  http    = require('http').Server(app),
  io      = require('socket.io')(http);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 io.on('connection', function(socket){
@@ -67,6 +76,18 @@ app.get('/teste', function(req, res){
 	io.emit('keyup', 'teste');
 	res.send('teste');
 });
+
+app.post('/export', function(req, res){
+
+  var describe = req.body.describe;
+  var id = req.body.id;
+
+  console.log(describe);
+  console.log(id);
+
+  res.send('ok');
+});
+
 
 http.listen(9000, function () {
  console.log('Server listening on *:9000');
