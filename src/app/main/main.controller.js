3e95b07da9140e15f7addc81d04fe0352b330abf
Eduardo Alves <edualvesguitar@gmail.com>
2015-11-29 18:01:27 -0200

    Removing html tags commented

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -511,14 +511,6 @@
       return line;
     };
 
-    vm.users = [
-      { id: 1, name: 'Scooby Doo' },
-      { id: 2, name: 'Shaggy Rodgers' },
-      { id: 3, name: 'Fred Jones' },
-      { id: 4, name: 'Daphne Blake' },
-      { id: 5, name: 'Velma Dinkley' }
-    ];
-
     vm.setActionLocator = function(action){
       $log.debug(action);
     };
